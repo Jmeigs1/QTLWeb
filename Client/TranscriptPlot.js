c8f0f7d0675ed6835cd8255c440baa7276b8b1fd
Jake Meigs <jakemeigs@gmail.com>
2019-09-17 10:52:30 -0400

    Adjust whitepace for transcript page

--- Client/TranscriptPlot.js
+++ Client/TranscriptPlot.js
@@ -24,8 +24,10 @@ const ArrowBox = styled.div`
     position: relative;
     text-align: center;
     padding: 4px 0;
-    width: 100px;
+    min-width: 100px;
     border: 2px solid #000;
+    box-sizing: padding-box;
+    padding: 0px 5px;
 
     &::after, &::before {
         top: 100%;
@@ -149,108 +151,114 @@ class TranscriptPlot extends Component {
 
         return (
             <div>
-                <h3 style={{textAlign:"center"}}>
-                    {this.props.header}
-                </h3>
-                <select
-                    size="3"
-                    value={sortedGeneData.map(o => o.name).includes(this.props.filterValue) ? this.props.filterValue : '' }
-                    onChange={(e) => {
-                        this.props.filterResultsFunc(e.target.value)
-                        }}>
-                    <option value="" disabled hidden>Choose here</option>
-                    {sortedGeneData.map(
-                        (gene) => (
-                            <option value={gene.name} key={gene.name}>{gene.name}</option>
-                        )
-                    )}
-                </select>
+                <div style={{display:'inline-block',width:'25%',position:'absolute'}}>
+                    <select
+                        size="3"
+                        value={sortedGeneData.map(o => o.name).includes(this.props.filterValue) ? this.props.filterValue : '' }
+                        onChange={(e) => {
+                            this.props.filterResultsFunc(e.target.value)
+                            }}>
+                        <option value="" disabled hidden>Choose here</option>
+                        {sortedGeneData.map(
+                            (gene) => (
+                                <option value={gene.name} key={gene.name}>{gene.name}</option>
+                            )
+                        )}
+                    </select>
+                </div>
+                <div style={{display:'inline-block',width:'100%'}}>    
+                    <h3 style={{textAlign:'center'}}>
+                        {this.props.header}
+                    </h3>
+                </div>
 
-                {
-                    this.state.toolTipText ? 
-                    (
+                <div style={{marginTop:'-16px'}}>
+                    {
+                        this.state.toolTipText ? 
+                        (
+                            <div 
+                                style={{
+                                    height:'32px',
+                                    margin: '10px auto',
+                                    width:width,
+                                }}>
+                                    <ArrowBox style={{left:this.state.toolTipOffset - 50}}>
+                                        {this.state.toolTipText}
+                                    </ArrowBox>
+                            </div>
+                        ) :
                         <div 
                             style={{
                                 height:'32px',
-                                margin: '20px auto',
-                                width:width,
-                            }}>
-                                <ArrowBox style={{left:this.state.toolTipOffset - 50}}>
-                                    {this.state.toolTipText}
-                                </ArrowBox>
-                        </div>
-                    ) :
-                    <div 
-                        style={{
-                            height:'32px',
-                            width:this.props.size[0],
-                            margin: '20px auto',
-                        }}/>
-                }
+                                width:this.props.size[0],
+                                margin: '10px auto',
+                            }}/>
+                    }
 
-                {layeredGeneData.map(
-                    (geneRow,i) => (
-                        <Svg 
-                            key={i}
-                            id={`TranscriptArea${i}`} 
-                            width={this.props.size[0]}
-                            height={this.props.size[1]}
-                            >
-                            <g transform= {`translate(${margin.left},0)`}>                        
-                                <line
-                                    x1 = {0}
-                                    x2 = {width}
-                                    y1 = {this.props.size[1]/2}
-                                    y2 = {this.props.size[1]/2}
-                                    strokeWidth = {1}
-                                    stroke = {'black'}
-                                />
-                            </g>
-                            <g width = {width} transform= {`translate(${margin.left},0)`}>
-                            {geneRow.map(
-                                (item) => {
-                                    let d3Data = this.props.d3Data,
-                                    fillcolor = this.props.filterValue == item.name ? 'brown' : 'black',
-                                    d3Width = d3Data.scaleX(item.end) - d3Data.scaleX(item.start),
-                                    start = d3Data.scaleX(item.start),
-                                    offset = start + d3Width/2
-                                    if(offset > width){
-                                        offset = width
-                                    }
-                                    else if(offset < 0){
-                                        offset = 0
-                                    }
-
-                                    return (
-                                    <TranscriptionRect
-                                        height = {this.props.size[1]}
-                                        width = {d3Width}
-                                        transform = {`translate(${start},0)`}
-                                        fill = {fillcolor}
-                                        key = {item.name}
-                                        data = {JSON.stringify(item)}
-                                        value={item.name}
-                                        onMouseEnter = {(e) => this.handleMouseOver(e,item.name,offset)}
-                                        onMouseLeave = {(e) => this.handleMouseOut(e,fillcolor)}
-                                        onClick={(e) => {this.props.filterResultsFunc(e.target.getAttribute("value"))}}
-                                        
+                    {layeredGeneData.map(
+                        (geneRow,i) => (
+                            <Svg 
+                                key={i}
+                                id={`TranscriptArea${i}`} 
+                                width={this.props.size[0]}
+                                height={this.props.size[1]}
+                                >
+                                <g transform= {`translate(${margin.left},0)`}>                        
+                                    <line
+                                        x1 = {0}
+                                        x2 = {width}
+                                        y1 = {this.props.size[1]/2}
+                                        y2 = {this.props.size[1]/2}
+                                        strokeWidth = {1}
+                                        stroke = {'black'}
                                     />
-                            )})}
-                            </g>
+                                </g>
+                                <g width = {width} transform= {`translate(${margin.left},0)`}>
+                                {geneRow.map(
+                                    (item) => {
+                                        let d3Data = this.props.d3Data,
+                                        fillcolor = this.props.filterValue == item.name ? 'brown' : 'black',
+                                        d3Width = d3Data.scaleX(item.end) - d3Data.scaleX(item.start),
+                                        start = d3Data.scaleX(item.start),
+                                        offset = start + d3Width/2
+                                        if(offset > width){
+                                            offset = width
+                                        }
+                                        else if(offset < 0){
+                                            offset = 0
+                                        }
+
+                                        return (
+                                        <TranscriptionRect
+                                            height = {this.props.size[1]}
+                                            width = {d3Width}
+                                            transform = {`translate(${start},0)`}
+                                            fill = {fillcolor}
+                                            key = {item.name}
+                                            data = {JSON.stringify(item)}
+                                            value={item.name}
+                                            onMouseEnter = {(e) => this.handleMouseOver(e,item.name,offset)}
+                                            onMouseLeave = {(e) => this.handleMouseOut(e,fillcolor)}
+                                            onClick={(e) => {this.props.filterResultsFunc(e.target.getAttribute("value"))}}
+                                            
+                                        />
+                                )})}
+                                </g>
+                        </Svg>
+                        )
+                    )}
+                    <Svg
+                        width={this.props.size[0]}
+                        height={40}>
+                        <g transform= {`translate(${margin.left},0)`}>
+                            <Axis {...axisPropsFromTickScale(this.props.d3Data.scaleX)} 
+                                style={{
+                                    orient: BOTTOM,
+                                }}
+                            />
+                        </g>
                     </Svg>
-                    )
-                )}
-                <Svg
-                    width={this.props.size[0]}
-                    height={40}>
-                    <g transform= {`translate(${margin.left},0)`}>
-                        <Axis {...axisPropsFromTickScale(this.props.d3Data.scaleX)} 
-                            style={{
-                                orient: BOTTOM,
-                            }}
-                        />
-                    </g>
-                </Svg>
+                </div>
             </div>
         )
     }
