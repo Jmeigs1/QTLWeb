7aa6dcd3011044c22a7440c5fdc88aa2a0a53b0d
Jake Meigs <jakemeigs@gmail.com>
2019-10-08 15:09:56 -0400

    Name fix

--- Client/App.js
+++ Client/App.js
@@ -62,7 +62,7 @@ class App extends Component {
                             render={
                                 ({ match }) => {
                                     return (
-                                        <VarientPage 
+                                        <VariantPage 
                                             geneSymbol={match.params.geneSymbol}
                                             site={match.params.site}
                                             chr={match.params.chr}
